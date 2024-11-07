704245e6a215436e8d9c1964bff9237c8db26c93
minhaz810 <chowdhuryminhaz021@gmail.com>
2024-11-08 04:49:19 +0600

    jwt added

--- ride/urls.py
+++ ride/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path, include
 from .views import TipsListView,AdminEarningListAPIView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('tips/', TipsListView.as_view()),
-    path('admin-earnings/',AdminEarningListAPIView.as_view())
+    path('admin-earnings/',AdminEarningListAPIView.as_view()),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
